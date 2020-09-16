6d63e64df94573b4cf5086b4a72c3d2b5bd982d7
leeminyeong <himinyeong2@naver.com>
2020-09-17 00:02:27 +0900

    0917

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('id-about-me').addEventListener('click',function(){
-    document.getElementById('main').style.display="none";
+    document.getElementById('home').style.display="none";
     document.getElementById('about-me').style.display="block";
 });
 
-document.getElementById('id-main').addEventListener('click',function(){
-    document.getElementById('main').style.display="block";
+document.getElementById('id-home').addEventListener('click',function(){
+    document.getElementById('home').style.display="block";
     document.getElementById('about-me').style.display="none";
 });
